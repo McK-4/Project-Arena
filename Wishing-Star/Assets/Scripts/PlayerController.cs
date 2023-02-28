1fcc9171423fbc468b826d9cd7bb73ced146a9b3
McK-4 <117216683+McK-4@users.noreply.github.com>
2023-02-28 13:39:02 -0800

    Basic Movement and Start of co-op multiplayer

---
+++ Wishing-Star/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    //Movement
+    public float movementSpeed = 7.5f;
+    Vector2 tempVel;
+    Vector2 moveinput = Vector2.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movement
+        //moveinput.y = Input.GetAxisRaw("Vertical");
+        //moveinput.x = Input.GetAxisRaw("Horizontal");
+
+        tempVel = rb.velocity;
+        tempVel.x = moveinput.x * movementSpeed;
+        tempVel.y = moveinput.y * movementSpeed;
+        rb.velocity = tempVel;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveinput = context.ReadValue<Vector2>();
+    }
+
+}
